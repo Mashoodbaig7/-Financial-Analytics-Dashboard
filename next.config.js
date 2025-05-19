6952f563bdf91df0004e3b56bf95d9fe3952b061
Mashoodbaig7 <you@example.com>
2025-05-19 22:43:39 +0500

    Add Netlify deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  // For Netlify deployment
+  trailingSlash: true,
+  output: 'export',
 }
 
 module.exports = nextConfig
